d7ddbddaded93d14687229ad5d4a7d35a9f180ea
Guillaume Lagorce <glagorce@turbulent.ca>
2022-03-11 12:36:53 -0500

    cover the get price by integrated tests

--- typescript/test/prices.spec.ts
+++ typescript/test/prices.spec.ts
@@ -23,13 +23,30 @@ describe('prices', () => {
         connection.close()
     });
 
-    it('does something', async () => {
+    [
+        { type: '1jour', age: undefined, cost: 35 },
+        { type: '1jour', age: 5, cost: 0 },
+        { type: '1jour', age: 14, cost: 25 },
+        { type: '1jour', age: 65, cost: 27 },
+        { type: '1jour', age: 65, cost: 27 },
+        { type: '1jour', age: 42, cost: 35 },
+        { type: '1jour', age: 42, cost: 23, date: new Date('2022-03-14T12:00:00') },
+        { type: '1jour', age: 42, cost: 35, date: new Date('2019-02-18T12:00:00') },
+        { type: 'night', age: undefined, cost: 0 },
+        { type: 'night', age: 5, cost: 0 },
+        { type: 'night', age: 42, cost: 19 },
+        { type: 'night', age: 65, cost: 8 },
+    ].forEach(({ type, age, cost, date }) => {
+        it(`returns ${cost} when type is ${type} and age is ${age} and date is ${date}`, async () => {
+            let query = `/prices?type=${type}`;
+            query = age ? `${query}&age=${age}` : query;
+            query = date ? `${query}&date=${date.toISOString()}` : query;
 
-        const response = await request(app)
-            .get('/prices?type=1jour')
+            const response = await request(app).get(query);
 
-        var expectedResult = { cost: 35 }
-        expect(response.body).deep.equal(expectedResult)
+            const expectedResult = { cost }
+            expect(response.body).deep.equal(expectedResult)
+        });
     });
 
 });
